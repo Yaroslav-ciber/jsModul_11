824f301967a17ed34d24025cb873febb98e6bd8e
Yaroslav <64233157+Yaroslav-ciber@users.noreply.github.com>
2021-04-07 19:30:47 +0300

    jsModul_11.io

---
+++ timer/index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    refs.timer = document.querySelector(selector);
+    this.targetDate = targetDate;
+  }
+  createTimer() {
+    setInterval(() => {
+      let currentDate = new Date();
+      let time = this.targetDate - currentDate;
+
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+      const secs = Math.floor((time % (1000 * 60)) / 1000);
+      this.changeInterface(days, hours, mins, secs);
+    }, 1000);
+  }
+  changeInterface(days, hours, mins, secs) {
+    refs.days.textContent = pad(days);
+    refs.hours.textContent = pad(hours);
+    refs.mins.textContent = pad(mins);
+    refs.secs.textContent = pad(secs);
+  }
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("April 07, 2021"),
+});
+
+document.addEventListener("load", timer.createTimer());
